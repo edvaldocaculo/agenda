798ffa8cee1f169645dcdc4d82faa610eab0f3a9
Edvaldo <edvaldocaculo2@gmail.com>
2024-01-23 22:14:20 -0300

    atualização

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Contact, Category
 
-# Register your models here now. 
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name','last_name', 'phone','show',)
+    list_display_links = ('id', 'first_name',)
+    ordering = ('-id',)
+    search_fields = ('first_name', 'last_name',)
+    list_filter = 'created_date',
+    list_per_page=10
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= 'name',
